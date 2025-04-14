12168453a633bc9a3ba5abe936feb77395660899
Harsh Tripathi <harshtripathi042@gmail.com>
2025-04-14 11:11:43 +0530

    useSetRecoilState to change the state of an atom

--- week 11/recoil/src/App.jsx
+++ week 11/recoil/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { RecoilRoot, useRecoilValue } from 'recoil'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
 import { jobsAtom, messagingAtom, networkAtom, notificationAtom } from './atoms'
 
 function App() {
@@ -27,10 +27,19 @@ function MainApp(){
        <button>Jobs ({jobsAtomCount})</button>
        <button>Messaging ({messagingAtomCount})</button>
        <button>Notifications ({notificationAtomCount})</button>
-       <button>Me</button>
+       <ButtonUpdater/>
       </div>
     </>
   )
+}
+
+function ButtonUpdater(){
+  const setMessagingAtomCount = useSetRecoilState(messagingAtom);
+  return <button onClick={()=>{
+    setMessagingAtomCount(c=>c+1);
+  }}>
+    Click to increment message
+  </button>
 }
 
 export default App
