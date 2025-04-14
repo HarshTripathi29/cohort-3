37a33af91c7db0b2374e1e21808ed0706cad3064
Harsh Tripathi <harshtripathi042@gmail.com>
2025-04-14 10:46:30 +0530

    basic recoil app

---
+++ week 11/recoil/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { RecoilRoot, useRecoilValue } from 'recoil'
+import { jobsAtom, messagingAtom, networkAtom, notificationAtom } from './atoms'
+
+function App() {
+
+  return <RecoilRoot>
+    <MainApp/>
+  </RecoilRoot>
+}
+
+function MainApp(){
+
+  const networkNotificationCount = useRecoilValue(networkAtom);
+  const jobsAtomCount = useRecoilValue(jobsAtom);
+  const notificationAtomCount = useRecoilValue(notificationAtom);
+  const messagingAtomCount = useRecoilValue(messagingAtom);
+
+
+  return (
+    <>
+      <div>
+       <button>My Network ({networkNotificationCount>=100?"99+":networkNotificationCount})</button>
+       <button>Jobs ({jobsAtomCount})</button>
+       <button>Messaging ({messagingAtomCount})</button>
+       <button>Notifications ({notificationAtomCount})</button>
+       <button>Me</button>
+      </div>
+    </>
+  )
+}
+
+export default App
