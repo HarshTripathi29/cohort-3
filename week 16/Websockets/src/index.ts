a79deb0cb2b77b21569d2935b568cf629886bb87
Harsh Tripathi <harshtripathi042@gmail.com>
2025-04-16 22:54:59 +0530

    comments in the ws server code

--- week 16/Websockets/src/index.ts
+++ week 16/Websockets/src/index.ts
@@ -1,17 +1,21 @@
+// Import the WebSocketServer class from the 'ws' library
 import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({port:8080});
+// Create a new WebSocket server that listens on port 8080
+const wss = new WebSocketServer({ port: 8080 });
 
-// event handler 
-
-wss.on("connection", function(socket){
+// Handle connection event when a client connects to the server
+wss.on("connection", function (socket) {
     console.log("user connected");
-// server sending a response after an interval 
-    setInterval(()=>{
-        socket.send("Current price of solana is "+Math.random());
-    },1000)
 
-    socket.on("message", (e)=>{
-        console.log(e.toString());
-    })
-})
+    // Send a message to the client every 1 second (1000 ms)
+    // This message simulates a changing price of Solana (random value)
+    setInterval(() => {
+        socket.send("Current price of solana is " + Math.random());
+    }, 1000);
+
+    // Handle incoming messages from the client
+    socket.on("message", (e) => {
+        console.log(e.toString()); // Log the message received from the client
+    });
+});
